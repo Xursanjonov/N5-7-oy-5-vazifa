a467e55511db7c663c598c5b2e2c4080a6765d08
Nusratillo <nxursanjonov888@gmail.com>
2024-07-05 22:55:59 +0500

    tsx add axios

--- src/components/products/ProductItem.tsx
+++ src/components/products/ProductItem.tsx
@@ -1,13 +1,30 @@
-import { memo } from "react";
-import cart from "../../assets/images/product-img.svg";
+import React, { memo } from "react";
+import productImage from "../../assets/images/product-img.svg";
 import "./products.css";
 
-const ProductItem = () => {
+interface Product {
+  id: number;
+  title: string;
+  price: string;
+  category: string;
+  description: string;
+  image: string;
+}
+
+interface ProductItemProps {
+  product: Product;
+}
+
+const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
   return (
     <div className="product-item">
-      <img className="product-item-img" src={cart} alt="" />
+      <img
+        className="product-item-img"
+        src={productImage}
+        alt={product?.title}
+      />
       <h4>Louis XVI ATHOS</h4>
-      <h5>165 000 руб.</h5>
+      <h5>{product?.price}pyб.</h5>
     </div>
   );
 };
--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,21 +1,41 @@
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
+import axios from "axios";
 import ProductItem from "./ProductItem";
 import "./products.css";
 
+interface Product {
+  id: number;
+  title: string;
+  price: string;
+  category: string;
+  description: string;
+  image: string;
+}
+
 const Products = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get<Product[]>("https://fakestoreapi.com/products?limit=8");
+      setProducts(response.data);
+    } catch (error) {
+      console.log(error, "Ma'lumotlarni yuklashda xatolik yuz berdi.");
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="products-wrapper">
       <h1>новые поступления</h1>
       <p className="line"></p>
       <div className="products-wrapper-items">
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
-        <ProductItem />
+        {products?.map((product) => (
+          <ProductItem key={product?.id} product={product} />
+        ))}
       </div>
     </div>
   );
